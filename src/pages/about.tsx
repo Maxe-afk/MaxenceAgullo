b243cafaaf49748bd655bee632acef43c13ea9ba
Maxence Agullo <magullo35@gmail.com>
2025-04-10 16:33:33 +0200

    corrected errors

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -4,7 +4,9 @@ function About() {
     return (
         <div className='backgroundContainer2'>
             <div className='CenterText'>
-                <p>Bonjour !<br />Je suis Maxence, développeur web full stack. Après une license de sociologie j'ai décidé de prendre une année sabatique pour me rediriger dans le domaine qui me plaît vraiment: celui de l'informatique. Ayant déjà fait du développement plus jeune j'ai eu envie de me tourner dans le développement web dont j'entendais de plus en plus parler autour de moi.<br /><br /> Je suis actuellemnt en formation au sein de la Wild Code School sur le campus de Lille jusqu'à Août 2025. Je vous présente ici mon portfolio, vous pouvez y accéder à partir du bouton "Projects" en haut de la page. J'espère que mes projets vous plairont autant qu'ils m'ont plu à les réaliser.</p>
+                <div>
+                    <p>Bonjour !<br />Je suis Maxence, développeur web full stack. Après une licence de sociologie j'ai décidé de prendre une année sabatique pour me rediriger dans le domaine qui me plaît vraiment: celui de l'informatique. Ayant déjà fait du développement plus jeune j'ai eu envie de me tourner dans le développement web dont j'entendais de plus en plus parler autour de moi.<br /><br /> Je suis actuellemnt en formation au sein de la Wild Code School sur le campus de Lille jusqu'à Août 2025. Je vous présente ici mon portfolio, vous pouvez y accéder à partir du bouton "Projects" en haut de la page. J'espère que mes projets vous plairont autant qu'ils m'ont plu à les réaliser.</p>
+                </div>
             </div>
         </div>
     )
